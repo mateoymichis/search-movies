a6bf086a0f5ebf42c4df9af65660c91933dd72f6
Mateo Fernandez <mateo.rost@gmail.com>
2023-03-18 18:14:04 -0300

    fetching a omdbapi

---
+++ src/Counter.svelte
@@ -0,0 +1,24 @@
+<script>
+    export let initialCounter = 0;
+    export let maxCounter = 9;
+    let contador = initialCounter;
+
+    function handleClick () {
+        contador++;
+    }
+
+    $: isEvenOrOdd = contador % 2 === 0 ? 'Is Even' : 'Is Odd';
+    $: if (contador > maxCounter) {
+        contador = maxCounter;
+    }
+</script>
+
+<style>
+    span {
+        color: red;
+    }
+</style>
+
+<button on:click={handleClick}>Incrementar</button>
+<span>{contador}</span>
+<span>{isEvenOrOdd}</span>
---
+++ src/Input.svelte
@@ -0,0 +1,16 @@
+<script>
+    import {API_KEY} from '../key.js';
+    let value = '';
+
+    const handleInput = (event) => value = event.target.value;
+
+    $: if (value.length > 2){
+       fetch(`https://www.omdbapi.com/?s=${value}&apikey=${API_KEY}`)
+       .then(res => res.json())
+       .then(apiResponse => {
+            response = apiResponse.Search;
+       })
+    }
+</script>
+
+<input value={value} on:input={handleInput} type="text">
